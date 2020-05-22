a912b8ddc28d77ccb4d61fca281f3eca530d79b6
mxmCherry <mxmCherry@gmail.com>
2020-05-22 17:15:43 +0300

    bps: more precise GenClientID comment

--- bps.go
+++ bps.go
@@ -4,8 +4,8 @@ import "github.com/google/uuid"
 
 // GenClientID generates random client ID.
 // It is guaranteed to:
-// only start with [a-z],
-// only contain [0-9A-Za-z_-]
+// start with "bps-",
+// only contain [0-9A-Za-z-]
 func GenClientID() string {
 	// uuid.New may panic only if random generator failures,
 	// which should be very rare (if happen at all):
