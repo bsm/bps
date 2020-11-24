5c88098514b88cde637cda77447d59ae11d8ee90
John Medforth <john.medforth@blacksquaremedia.com>
2020-11-24 11:28:53 +0000

    Updating the defaults.

--- ruby/bps-kafka/lib/bps/publisher/kafka_async.rb
+++ ruby/bps-kafka/lib/bps/publisher/kafka_async.rb
@@ -14,10 +14,10 @@ module BPS
       # @param [Hash] opts the options.
       # @option opts [Integer] :max_queue_size (defaults to: 1000)
       #                        the maximum number of messages allowed in the queue.
-      # @option opts [Integer] :delivery_threshold (defaults to: 0)
+      # @option opts [Integer] :delivery_threshold (defaults to: 1000)
       #                        if greater than zero, the number of buffered messages that will automatically
       #                        trigger a delivery.
-      # @option opts [Integer] :delivery_interval (defaults to: 0) if greater than zero, the number of
+      # @option opts [Integer] :delivery_interval (defaults to: 30) if greater than zero, the number of
       #                        seconds between automatic message deliveries.
       def initialize(broker_addrs, **opts) # rubocop:disable Lint/UselessMethodDefinition
         super
@@ -25,7 +25,7 @@ module BPS
 
       private
 
-      def init_producer(max_queue_size: 1000, delivery_threshold: 0, delivery_interval: 0)
+      def init_producer(max_queue_size: 1000, delivery_threshold: 1000, delivery_interval: 30)
         @client.async_producer(
           max_queue_size: max_queue_size,
           delivery_threshold: delivery_threshold,
